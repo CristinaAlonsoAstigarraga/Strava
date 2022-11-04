bf53b49f17775ff73acc53d0ee6794d42137b35c
Cristina Alonso <cris.alonso@opendeusto.es>
2022-11-04 10:22:45 +0100

    Clases ventanas vacías

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -1,5 +1,14 @@
 package clases;
 
+import java.util.Date;
+
 public class Usuario {
 
+	private String nombre;
+	private Date fechaNac;
+	private float peso;
+	private float altura;
+	private float fcm;	//Frecuencia cardiaca máxima
+	private float fcr;	//Frecuencia cardiaca reposo
+	
 }

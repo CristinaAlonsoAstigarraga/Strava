fe5689e6de897328972b24f842ea10016775e5fe
Cristina Alonso <cris.alonso@opendeusto.es>
2022-11-04 11:59:45 +0100

    Boceto VentanaObtenerRetosActivos

--- src/ventanas/VentanaConsultaEstadoRetos.java
+++ src/ventanas/VentanaConsultaEstadoRetos.java
@@ -1,8 +1,10 @@
 package ventanas;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class VentanaConsultaEstadoRetos {
 
@@ -38,6 +40,21 @@ public class VentanaConsultaEstadoRetos {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panelNorte = new JPanel();
+		frame.getContentPane().add(panelNorte, BorderLayout.NORTH);
+		
+		JPanel panelSur = new JPanel();
+		frame.getContentPane().add(panelSur, BorderLayout.SOUTH);
+		
+		JPanel panelOeste = new JPanel();
+		frame.getContentPane().add(panelOeste, BorderLayout.WEST);
+		
+		JPanel panelEste = new JPanel();
+		frame.getContentPane().add(panelEste, BorderLayout.EAST);
+		
+		JPanel panelCentro = new JPanel();
+		frame.getContentPane().add(panelCentro, BorderLayout.CENTER);
 	}
 
 }
--- src/ventanas/VentanaObtenerRetosActivos.java
+++ src/ventanas/VentanaObtenerRetosActivos.java
@@ -3,10 +3,18 @@ package ventanas;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import java.awt.GridLayout;
 
 public class VentanaObtenerRetosActivos {
 
 	private JFrame frame;
+	private JTextField textFieldFecha;
 
 	/**
 	 * Launch the application.
@@ -36,8 +44,49 @@ public class VentanaObtenerRetosActivos {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(150, 150, 675, 350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panelNorte = new JPanel();
+		frame.getContentPane().add(panelNorte, BorderLayout.NORTH);
+		
+		JButton btnVolver = new JButton("VOLVER");
+		panelNorte.add(btnVolver);
+		
+		JLabel lblTitulo = new JLabel("OBTENER INFORMACIÓN RETOS ACTIVOS");
+		panelNorte.add(lblTitulo);
+		
+		JButton btnLogOut = new JButton("LOG OUT");
+		panelNorte.add(btnLogOut);
+		
+		JPanel panelSur = new JPanel();
+		frame.getContentPane().add(panelSur, BorderLayout.SOUTH);
+		
+		JButton btnBuscar = new JButton("BUSCAR");
+		panelSur.add(btnBuscar);
+		
+		JPanel panelOeste = new JPanel();
+		frame.getContentPane().add(panelOeste, BorderLayout.WEST);
+		
+		JPanel panelEste = new JPanel();
+		frame.getContentPane().add(panelEste, BorderLayout.EAST);
+		
+		JPanel panelCentro = new JPanel();
+		frame.getContentPane().add(panelCentro, BorderLayout.CENTER);
+		panelCentro.setLayout(new GridLayout(0, 2, 0, 10));
+		
+		JLabel lblFecha = new JLabel("FECHA:");
+		panelCentro.add(lblFecha);
+		
+		textFieldFecha = new JTextField();
+		panelCentro.add(textFieldFecha);
+		textFieldFecha.setColumns(10);
+		
+		JLabel lblNombreRetos = new JLabel("NOMBRE RETO:");
+		panelCentro.add(lblNombreRetos);
+		
+		JComboBox comboBoxRetosAcitvos = new JComboBox();
+		panelCentro.add(comboBoxRetosAcitvos);
 	}
 
 }

6624b27e5e29f29cf34fc147815983cecd6a8d72
Cristina Alonso <cris.alonso@opendeusto.es>
2022-11-06 16:02:08 +0100

    Boceto VentanaCreacionReto

--- src/ventanas/VentanaCreacionReto.java
+++ src/ventanas/VentanaCreacionReto.java
@@ -3,10 +3,22 @@ package ventanas;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 public class VentanaCreacionReto {
 
 	private JFrame frame;
+	private JTextField textFieldNombre;
+	private JTextField textFieldFechaIni;
+	private JTextField textFieldFechaFin;
+	private JTextField textFieldDistancia;
+	private JTextField textFieldTiempoObjetivo;
 
 	/**
 	 * Launch the application.
@@ -38,6 +50,75 @@ public class VentanaCreacionReto {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panelNorte = new JPanel();
+		frame.getContentPane().add(panelNorte, BorderLayout.NORTH);
+		
+		JButton btnVolver = new JButton("VOLVER");
+		panelNorte.add(btnVolver);
+		
+		JLabel lblTitulo = new JLabel("CREACIÓN DE UN RETO");
+		panelNorte.add(lblTitulo);
+		
+		JButton btnLogOut = new JButton("LOG OUT");
+		panelNorte.add(btnLogOut);
+		
+		JPanel panelSur = new JPanel();
+		frame.getContentPane().add(panelSur, BorderLayout.SOUTH);
+		
+		JButton btnCrear = new JButton("CREAR");
+		panelSur.add(btnCrear);
+		
+		JPanel panelOeste = new JPanel();
+		frame.getContentPane().add(panelOeste, BorderLayout.WEST);
+		
+		JPanel panelEste = new JPanel();
+		frame.getContentPane().add(panelEste, BorderLayout.EAST);
+		
+		JPanel panelCentro = new JPanel();
+		frame.getContentPane().add(panelCentro, BorderLayout.CENTER);
+		panelCentro.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JLabel lblNombre = new JLabel("NOMBRE: ");
+		panelCentro.add(lblNombre);
+		
+		textFieldNombre = new JTextField();
+		panelCentro.add(textFieldNombre);
+		textFieldNombre.setColumns(10);
+		
+		JLabel lblFechaIni = new JLabel("FECHA INICIO:");
+		panelCentro.add(lblFechaIni);
+		
+		textFieldFechaIni = new JTextField();
+		panelCentro.add(textFieldFechaIni);
+		textFieldFechaIni.setColumns(10);
+		
+		JLabel lblFechaFin = new JLabel("FECHA FIN:");
+		panelCentro.add(lblFechaFin);
+		
+		textFieldFechaFin = new JTextField();
+		panelCentro.add(textFieldFechaFin);
+		textFieldFechaFin.setColumns(10);
+		
+		JLabel lblDistancia = new JLabel("DISTANCIA");
+		panelCentro.add(lblDistancia);
+		
+		textFieldDistancia = new JTextField();
+		panelCentro.add(textFieldDistancia);
+		textFieldDistancia.setColumns(10);
+		
+		JLabel lblTiempoObjetivo = new JLabel("TIEMPO OBJETIVO");
+		panelCentro.add(lblTiempoObjetivo);
+		
+		textFieldTiempoObjetivo = new JTextField();
+		panelCentro.add(textFieldTiempoObjetivo);
+		textFieldTiempoObjetivo.setColumns(10);
+		
+		JLabel lblDeporte = new JLabel("DEPORTE");
+		panelCentro.add(lblDeporte);
+		
+		JComboBox comboBoxDeporte = new JComboBox();
+		panelCentro.add(comboBoxDeporte);
 	}
 
 }

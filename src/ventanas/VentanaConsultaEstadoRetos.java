772e427747e5c48f07b8691a58553ce452bfaf83
Cristina Alonso <cris.alonso@opendeusto.es>
2022-11-06 16:14:49 +0100

    Boceto VentanaConsultaEstadoRetosActivos

--- src/ventanas/VentanaConsultaEstadoRetos.java
+++ src/ventanas/VentanaConsultaEstadoRetos.java
@@ -3,12 +3,22 @@ package ventanas;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class VentanaConsultaEstadoRetos {
 
 	private JFrame frame;
+	private JTable table;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,11 +48,20 @@ public class VentanaConsultaEstadoRetos {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(150, 150, 675, 350);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panelNorte = new JPanel();
 		frame.getContentPane().add(panelNorte, BorderLayout.NORTH);
+		
+		JButton btnVolver = new JButton("VOLVER");
+		panelNorte.add(btnVolver);
+		
+		JLabel lblTitulo = new JLabel("CONSULTAR ESTADOS RETOS ACEPTADOS");
+		panelNorte.add(lblTitulo);
+		
+		JButton btnLogOut = new JButton("LOG OUT");
+		panelNorte.add(btnLogOut);
 		
 		JPanel panelSur = new JPanel();
 		frame.getContentPane().add(panelSur, BorderLayout.SOUTH);
@@ -50,11 +69,36 @@ public class VentanaConsultaEstadoRetos {
 		JPanel panelOeste = new JPanel();
 		frame.getContentPane().add(panelOeste, BorderLayout.WEST);
 		
+		JLabel lblFecha = new JLabel("FECHA:");
+		panelOeste.add(lblFecha);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		panelOeste.add(textField);
+		
 		JPanel panelEste = new JPanel();
 		frame.getContentPane().add(panelEste, BorderLayout.EAST);
 		
 		JPanel panelCentro = new JPanel();
 		frame.getContentPane().add(panelCentro, BorderLayout.CENTER);
+		panelCentro.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"NOMBRE", "ESTADO", "DESCRIPCIÓN", "PORCENTAJE COMPLETADO"},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"NOMBRE", "ESTADO", "DESCRIPCIÓN", "PORCENTAJE COMPLETADO"
+			}
+		));
+		table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panelCentro.add(table);
 	}
 
 }

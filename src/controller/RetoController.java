6ab7bf22e99a3204a2b6cc40be90f4e1e07ad2f9
Cristina Alonso <cris.alonso@opendeusto.es>
2022-11-24 00:09:30 +0100

    RetoController

--- src/controller/RetoController.java
+++ src/controller/RetoController.java
@@ -1,5 +1,60 @@
 package controller;
 
-public class RetoController {
+import remote.ServiceLocator;
 
+import java.rmi.RemoteException;
+import java.util.List;
+
+import clases.Reto;
+import dto.RetoDTO;
+import remote.IFachadaRemota;
+
+public class RetoController {
+	
+	private ServiceLocator serviceLocator;
+	
+	public RetoController(ServiceLocator serviceLocator) {
+		this.serviceLocator = serviceLocator;
+	}
+	
+	public List<RetoDTO> getRetos(long token){		//No lo hemos puesto en el diagrama, hay que ponerlo?
+
+		try {
+			return this.serviceLocator.getService().getRetos(token);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			System.out.println("# Error obteniendo todos los retos: " + e);
+			return null;
+		}
+		
+	}
+	
+	public Reto crearReto(long token, RetoDTO nuevoReto) {
+		try {
+			return this.serviceLocator.getService().crearReto(token, nuevoReto);
+		} catch (RemoteException e) {
+			System.out.println("# Error creando el reto: " + e);
+			return null;
+		}
+	}
+
+	public List<RetoDTO> obtenerRetosActivos(long token){
+		try {
+			return this.serviceLocator.getService().obtenerRetosActivos(token);
+		} catch (RemoteException e) {
+			System.out.println("# Error obteniendo los retos activos: " + e);
+			return null;
+		}
+	}
+	
+	public boolean aceptarReto(long token, String nombre) {
+		try {
+			return this.serviceLocator.getService().aceptarReto(token, nombre);
+		} catch (RemoteException e) {
+			System.out.println("# Error aceptando el reto: " + e);
+			return false;
+		}
+	}
+	
+	//Falta consultarReto - lo hemos puesto en el diagrama
 }

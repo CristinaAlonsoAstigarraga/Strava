85fa29f616224b02532e2b5cfa04347af8878daf
Cristina Alonso <cris.alonso@opendeusto.es>
2022-11-24 00:13:08 +0100

    SesionEntrenamientoController

--- src/controller/SesionEntrenamientoController.java
+++ src/controller/SesionEntrenamientoController.java
@@ -1,5 +1,32 @@
 package controller;
 
+import java.rmi.RemoteException;
+import java.security.Provider.Service;
+import java.util.List;
+
+import clases.SesionEntrenamiento;
+import dto.SesionEntrenamientoDTO;
+import remote.ServiceLocator;
+
 public class SesionEntrenamientoController {
 
+	private ServiceLocator serviceLocator;
+	
+	public List<SesionEntrenamientoDTO> getSesiones(long token){	//No está en el diagrama
+		try {
+			return this.serviceLocator.getService().getSesiones(token);
+		} catch (RemoteException e) {
+			System.out.println("# Error obteniendo todas las sesiones: " + e);
+			return null;
+		}
+	}
+	
+	public SesionEntrenamiento crearSesionEntrenamiento(long token, SesionEntrenamientoDTO nuevaSesion) {
+		try {
+			return this.serviceLocator.getService().crearSesionEntrenamiento(token, nuevaSesion);
+		} catch (RemoteException e) {
+			System.out.println("# Error creando la sesión de entrenamiento: " + e);
+			return null;
+		}
+	}
 }

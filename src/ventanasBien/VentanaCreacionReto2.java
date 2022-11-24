2cd5ccf60734d25b229af48731133e78c32e27b4
Cristina Alonso <cris.alonso@opendeusto.es>
2022-11-24 10:38:46 +0100

    Arreglo VentanaCrearReto

--- src/ventanasBien/VentanaCreacionReto2.java
+++ src/ventanasBien/VentanaCreacionReto2.java
@@ -45,7 +45,7 @@ public class VentanaCreacionReto2 extends JFrame {
 	 */
 	public VentanaCreacionReto2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 650, 300);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
